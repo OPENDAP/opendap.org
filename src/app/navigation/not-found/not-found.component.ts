e5caa7bc71d2cd5264301c90642c8144c8c642fb
Alexander Porrello <alexporrello@gmail.com>
2020-12-29 11:39:55 -0600

    Pulled 404 page out of 2012.

--- src/app/navigation/not-found/not-found.component.ts
+++ src/app/navigation/not-found/not-found.component.ts
@@ -2,8 +2,57 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-not-found',
-  templateUrl: './not-found.component.html',
-  styleUrls: ['./not-found.component.scss']
+  template: `
+  <div class="not-found">
+    <img src="../../../assets/images/hal_flat.png" alt="Oops!">
+
+    <div>
+      <h1>404 - Not Found</h1>
+      <h2>I'm sorry, Dave. I'm afraid I can't do that.</h2>
+
+      <p>
+        We couldn't find (maybe lost?) the page you were looking for.
+        Try out one of the links below to assist you on
+        your journey, or reach out to support@opendap.org for assistance:
+      </p>
+      <ul>
+        <li><a routerLink=''>Home</a></li>
+        <li><a routerLink='/software'>Software</a></li>
+        <li><a routerLink='/support'>Support</a></li>
+        <li><a routerLink='/support/faq'>FAQ</a></li>
+        <li><a routerLink='/about'>About OPeNDAP</a></li>
+      </ul>
+    </div>
+  </div>
+  `,
+  styles: [`
+    .not-found {
+      display: flex;
+      max-width: 800px;
+      margin: auto;
+    }
+
+    img {
+      width: 300px;
+      height: 300px;
+    }
+
+    p {
+      text-align: justify;
+    }
+
+    h1 {
+      font-weight: 300;
+      margin-bottom: 0;
+      font-size: 2em;
+      line-height: 1.5em;
+    }
+
+    h2 {
+      font-family: 'Roboto Slab', serif;
+      font-weight: 300;
+    }
+  `]
 })
 export class NotFoundComponent implements OnInit {
 
