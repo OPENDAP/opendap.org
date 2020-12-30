9ebb4185092a4858880c7f63aaa4cda73678f228
Alexander Porrello <alexporrello@gmail.com>
2020-12-30 11:12:18 -0600

    Moved componenet to its own webpage.

--- src/app/hyrax/documentation/documentation.component.ts
+++ src/app/hyrax/documentation/documentation.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
 
 @Component({
   selector: 'app-documentation',
-  templateUrl: './documentation.component.html',
-  styleUrls: ['./documentation.component.scss']
+  template: `
+    <iframe
+      id="documentation"
+      src="https://opendap.github.io/hyrax_guide/Master_Hyrax_Guide.html"
+      title="The Hyrax Data Server Installation and Configuration Guide">
+    </iframe>
+  `,
+  styles: [`
+    iframe {
+      margin-top: 50px;
+      border: 0;
+      width: 100%;
+    }
+  `]
 })
-export class DocumentationComponent implements OnInit {
+export class DocumentationComponent implements AfterViewInit {
 
   constructor() { }
 
-  ngOnInit(): void {
+  ngAfterViewInit(): void {
+    const iFrame = document.getElementById('documentation');
+    console.log(iFrame);
   }
-
 }
