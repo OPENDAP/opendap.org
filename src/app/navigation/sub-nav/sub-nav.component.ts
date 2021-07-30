ac411db62235c89e260b86997a083c7349f8bc53
Alexander Porrello <alexander.porrello@kapsch.net>
2021-07-30 17:35:36 -0500

    Added scroll animations to sub nav.

--- src/app/navigation/sub-nav/sub-nav.component.ts
+++ src/app/navigation/sub-nav/sub-nav.component.ts
@@ -9,6 +9,44 @@ import { Heading } from 'src/app/shared/components/adoc-template/adoc-template.c
 export class SubNavComponent  {
   @Input() pageTitle: string;
   @Input() headings: Heading[];
+  @Input() scrollOffset = 25;
 
-  constructor() { }
+  activeSection: any;
+
+  constructor() {
+    window.addEventListener('scroll', () => {
+      for (let i = 0; i < this.headings.length; i++) {
+        if (document.getElementById(this.headings[i].id) !== null) {
+          if (this.headings[i + 1] === undefined) {
+            this.activeSection = this.headings[i];
+            break;
+          }
+
+          let a = window.scrollY + this.scrollOffset + 15 > document.getElementById(this.headings[i].id).offsetTop;
+          let b = window.scrollY + this.scrollOffset + 15 < document.getElementById(this.headings[i + 1].id).offsetTop;
+
+          if (a && b) {
+            this.activeSection = this.headings[i];
+            break;
+          }
+        }
+      }
+    });
+  }
+
+  public scrollIntoView(heading: Heading) {
+    this.activeSection = heading;
+
+    window.scrollTo({
+      top: document.getElementById(heading.id).offsetTop - this.scrollOffset,
+      behavior: 'smooth'
+    });
+  }
+
+  public scrollToTop() {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth'
+    })
+  }
 }
